850dbc505642664bf9091da461f412aea4e41084
maj-rf <rmkfermin@gmail.com>
2025-06-03 10:53:59 +0800

    feat: add checking for Database errors (partial)

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,5 +1,24 @@
 import { type ErrorRequestHandler, type Request, type Response, type NextFunction } from 'express';
 import { isHttpError } from 'http-errors';
+import { DatabaseError } from 'pg';
+
+const handleDatabaseError = (err: DatabaseError) => {
+  switch (err.code) {
+    case '22P02':
+      return {
+        status: 400,
+        message: `Malformatted params: ${err.file}`,
+      };
+    case '23503':
+      return {
+        status: 400,
+        message: `Related resource not found. Cannot modify: ${err.message}`,
+      };
+    default:
+      // handling all other errors
+      return { status: 500, message: `Something went wrong: ${err.message}` };
+  }
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error: unknown,
@@ -13,6 +32,11 @@ export const errorHandler: ErrorRequestHandler = (
   if (isHttpError(error)) {
     status = error.status;
     errorMessage = error.message;
+  }
+  if (error instanceof DatabaseError) {
+    const { status: dbStatus, message: dbMessage } = handleDatabaseError(error);
+    status = dbStatus;
+    errorMessage = dbMessage;
   }
   res.status(status).json({ message: errorMessage });
   next();
