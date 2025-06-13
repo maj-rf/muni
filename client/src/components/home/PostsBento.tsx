aaac24692b904417740a2fc047f42d9cd54b641b
maj-rf <rmkfermin@gmail.com>
2025-06-13 19:36:39 +0800

    fix: make bento simpler and add loading skeleton

--- client/src/components/home/PostsBento.tsx
+++ client/src/components/home/PostsBento.tsx
@@ -1,16 +1,39 @@
 import { PostsBentoItem } from './PostsBentoItem';
 import { useGetRecentPosts } from '@/hooks/usePost';
-import { Loading } from '../common/Loading';
+import { Skeleton } from '../ui/skeleton';
+const PostBentoSkeleton = () => {
+  return (
+    <div className="py-2 lg:px-4">
+      <div className="w-full grid grid-cols-1 auto-rows-[360px] lg:grid-cols-10 gap-2">
+        <div className="lg:col-span-3 lg:row-span-1 h-full">
+          <Skeleton className="w-full h-full" />
+        </div>
+        <div className="lg:col-span-4 lg:row-span-2 h-full">
+          <Skeleton className="w-full h-full" />
+        </div>
+        <div className="lg:col-span-3 lg:row-span-1 h-full">
+          <Skeleton className="w-full h-full" />
+        </div>
+        <div className="lg:col-span-3 lg:row-span-1 h-full">
+          <Skeleton className="w-full h-full" />
+        </div>
+        <div className="lg:col-span-3 lg:row-span-1 h-full">
+          <Skeleton className="w-full h-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const PostsBento = () => {
   const { data, isPending, error } = useGetRecentPosts();
 
-  if (isPending) return <Loading />;
+  if (isPending) return <PostBentoSkeleton />;
   if (error) return <div>Error fetching data</div>;
 
   return (
-    <div className="flex items-center justify-center p-4">
-      <div className="grid grid-cols-1 lg:grid-cols-10 lg:auto-rows-fr gap-1">
+    <div className="flex items-center justify-center py-2 lg:px-4">
+      <div className="w-full grid grid-cols-1 lg:grid-cols-10 auto-rows-[360px] gap-2">
         {data.map((post, index) => (
           <PostsBentoItem key={post.id} variant={index === 1 ? 'big' : 'small'} post={post} />
         ))}
